21cbc9936114b8d3a200836ef644f516c34f1945
Kamil-khanUQA <42178696+mkamilkhan@users.noreply.github.com>
2026-01-26 23:24:48 -0800

    Replace react-slick with Swiper.js for CategoriesSlider: fix mobile sliding, ensure proper responsive breakpoints (1 card mobile, 4-5 cards desktop), uniform card widths

--- client/src/components/CategoriesSlider.jsx
+++ client/src/components/CategoriesSlider.jsx
@@ -1,7 +1,9 @@
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
 import { Link } from "react-router-dom";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import "./CategoriesSlider.css";
 
 const CategoriesSlider = ({ categories = [] }) => {
@@ -9,23 +11,42 @@ const CategoriesSlider = ({ categories = [] }) => {
     return <div className="no-categories">No categories available</div>;
   }
 
-  const sliderSettings = {
-    slidesToShow: 4, // desktop
-    slidesToScroll: 1,
-    infinite: true,
-    dots: true,
-    arrows: false,
-    responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 2 } },
-      { breakpoint: 768, settings: { slidesToShow: 1 } },
-    ],
-  };
-
   return (
     <div className="categories-slider-wrapper">
-      <Slider {...sliderSettings} className="categories-slick-slider">
+      <Swiper
+        modules={[Pagination, Navigation]}
+        spaceBetween={28}
+        slidesPerView={4}
+        pagination={{
+          clickable: true,
+          dynamicBullets: false,
+        }}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 16,
+          },
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 28,
+          },
+          1400: {
+            slidesPerView: 5,
+            spaceBetween: 28,
+          },
+        }}
+        className="categories-swiper"
+      >
         {categories.map((cat, idx) => (
-          <div key={idx} className="category-slide">
+          <SwiperSlide key={idx} className="category-slide">
             <div className="category-card-tour">
               <div className="category-image-wrapper">
                 <img src={cat.image} alt={cat.name} className="category-image" />
@@ -37,9 +58,9 @@ const CategoriesSlider = ({ categories = [] }) => {
                 </Link>
               </div>
             </div>
-          </div>
+          </SwiperSlide>
         ))}
-      </Slider>
+      </Swiper>
     </div>
   );
 };
